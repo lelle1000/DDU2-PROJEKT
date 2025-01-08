5f351bbebded9afdf4dd2d82abe53adaafaaf29a
lelle1000 <linusestrandberg@gmail.com>
2025-01-08 06:58:24 +0100

    Skapat den första delen i common.js och påbörjat att pilla med att fylla gridden med nummer.

--- common.js
+++ common.js
@@ -1,6 +1,18 @@
+/* Used functions */
+
+
+
+
+/* Element shortcuts */
+let a = document.createElement("a");
+let p = document.createElement("p");
+let div = document.createElement("div");
+let button = document.createElement("button");
+let input = document.createElement("input");
+
 
 /* Home Link on every document */
-let Home = document.createElement("a");
+const Home = document.createElement("a");
 document.body.append(Home);
 Home.textContent = "Home";
 Home.setAttribute("href", "../index.html");
@@ -8,5 +20,30 @@ Home.classList.add("FontClass")
 Home.id = "Home"
 
 
-/* How many numbers in the grid + Input + Create */
+/* How many numbers in the grid paragraph + Input + Create */
+const CommonContainer = div;
+document.body.append(CommonContainer);
+CommonContainer.id = "CommonContainer";
 
+const CommonParagraph = p;
+CommonContainer.append(CommonParagraph);
+CommonParagraph.textContent = "How many numbers in the grid?";
+CommonParagraph.classList.add("FontClass");
+
+const CommonInput = input;
+CommonContainer.append(CommonInput);
+CommonInput.classList.add("FontClass");
+CommonInput.style.textAlign = "center";
+CommonInput.id = "CommonInput";
+CommonInput.setAttribute("type", "number");
+
+const CommonButton = button;;
+CommonContainer.append(CommonButton);
+CommonButton.textContent = "Create";
+CommonButton.classList.add("FontClass");
+CommonButton.id = "CommonButton";
+
+/* Grid-Layout and Numbers */
+CommonButton.addEventListener("click" function () {
+    if ()
+})
