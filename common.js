afab4f5f4dc2eba0c74f4181f3be1e5f29c0ff4c
lelle1000 <linusestrandberg@gmail.com>
2025-01-08 05:10:19 +0100

    Skapat home-knappen med JS på vardera sida och gett den storlek och struktur

---
+++ common.js
@@ -0,0 +1,12 @@
+
+/* Home Link on every document */
+let Home = document.createElement("a");
+document.body.append(Home);
+Home.textContent = "Home";
+Home.setAttribute("href", "../index.html");
+Home.classList.add("FontClass")
+Home.id = "Home"
+
+
+/* How many numbers in the grid + Input + Create */
+
