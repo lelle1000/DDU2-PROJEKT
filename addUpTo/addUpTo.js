a6e20f8316470b67a36620bba0f0babe6d7cf7d2
lelle1000 <linusestrandberg@gmail.com>
2025-01-11 17:04:54 +0100

    Påbörjat AddUpTo sidan med en addEventlistener och function

--- addUpTo/addUpTo.js
+++ addUpTo/addUpTo.js
@@ -1,5 +1,11 @@
 
+
+/* Moved down the newly created HTML elements inbetween the Common JS elements */
 const AddUpContainer = document.getElementById("AddUpToControlsContainer");
 const gridContainer = document.getElementById("GridContainer");
 
 gridContainer.parentNode.insertBefore(AddUpContainer, gridContainer)
+
+document.getElementById("AddUpToButton").addEventListener("click", function () {
+    let numberBoxes = document.querySelectorAll(".NumberBoxes")
+})
